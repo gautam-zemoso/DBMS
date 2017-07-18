35946d1e6e5c072ad51ff4b07762aa5a610e1f4a
Gautam Kumar <gautam.kumar@zemosolabs.com>
2017-07-18 22:22:12 +0530

    cab rental Assignment

--- cab_rental/cab_rental.sql
+++ cab_rental/cab_rental.sql
@@ -2,27 +2,15 @@ DROP SCHEMA IF EXISTS cab_rental;
 CREATE SCHEMA IF NOT EXISTS cab_rental ;
 USE cab_rental ;
 
-CREATE TABLE IF NOT EXISTS country (
-  country_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  country VARCHAR(50) NOT NULL,
-  created_timestamp DATETIME NOT NULL, 
-  created_by_id INT NOT NULL, 
-  modified_by_id INT DEFAULT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (country_id)
-);
-
 CREATE TABLE IF NOT EXISTS city (
   city_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   city VARCHAR(50) NOT NULL,
-  country_id SMALLINT UNSIGNED NOT NULL,
+  country_name  VARCHAR(40) NOT NULL,
   created_timestamp DATETIME NOT NULL, 
   created_by_id INT NOT NULL, 
   modified_by_id INT DEFAULT NULL,
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (city_id),
-  KEY idx_fk_country_id (country_id),
-  CONSTRAINT fk_city_country FOREIGN KEY (country_id) REFERENCES country (country_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY  (city_id)
 );
 
 CREATE TABLE IF NOT EXISTS address (
@@ -75,8 +63,10 @@ CREATE TABLE IF NOT EXISTS driver (
   KEY idx_fk_address_id (address_id),
   CONSTRAINT fk_driver_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS cab (
   cab_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  driver_id SMALLINT UNSIGNED NOT NULL,
   cab_name VARCHAR(50) NOT NULL,
   no_of_seats SMALLINT NOT NULL,
   rate SMALLINT NOT NULL,
@@ -85,27 +75,20 @@ CREATE TABLE IF NOT EXISTS cab (
   created_by_id INT NOT NULL, 
   modified_by_id INT DEFAULT NULL,
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (cab_id)
-);
-CREATE TABLE IF NOT EXISTS cab_driver(
-  cab_id SMALLINT UNSIGNED NOT NULL,
-  driver_id SMALLINT UNSIGNED NOT NULL,
-  KEY idx_fk_driver_id (driver_id),
-  KEY idx_fk_cab_id (cab_id),
-  CONSTRAINT fk_cab_driver_driver_id FOREIGN KEY (driver_id) REFERENCES driver (driver_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_cab_driver_cab_id FOREIGN KEY (cab_id) REFERENCES cab (cab_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (cab_id),
+  CONSTRAINT UNIQUE uk_driver_id (driver_id),
+  CONSTRAINT fk_cab_driver FOREIGN KEY (driver_id) REFERENCES driver (driver_id) ON DELETE RESTRICT ON UPDATE CASCADE
   );
-CREATE TABLE booking(
-	book_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE IF NOT EXISTS booking(
   cab_id SMALLINT UNSIGNED NOT NULL,
   customer_id SMALLINT UNSIGNED NOT NULL,
 	book_from VARCHAR(45) NOT NULL,
 	book_to VARCHAR(45) NOT NULL,
 	book_date DATETIME NOT NULL,
-  PRIMARY KEY  (book_id),
-  KEY idx_fk_customer_id (customer_id),
+  CONSTRAINT UNIQUE uk_customer_id (customer_id),
   KEY idx_fk_cab_id (cab_id),
   CONSTRAINT fk_booking_driver_id FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT fk_booking_cab_id FOREIGN KEY (cab_id) REFERENCES cab (cab_id) ON DELETE RESTRICT ON UPDATE CASCADE
  
-)
+);
