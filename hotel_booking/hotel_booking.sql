148d7b166f3a6f1dc8bd118db38675edb5d01b32
Gautam Kumar <gautam.kumar@zemosolabs.com>
2017-07-24 09:33:46 +0530

    Hotel booking Assignment

--- hotel_booking/hotel_booking.sql
+++ hotel_booking/hotel_booking.sql
@@ -2,62 +2,30 @@ DROP SCHEMA IF EXISTS hotel_booking;
 CREATE SCHEMA IF NOT EXISTS hotel_booking ;
 USE hotel_booking ;
 
-
-CREATE TABLE IF NOT EXISTS country (
-  country_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  country VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS loyal_customer(
+  registration_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   created_timestamp DATETIME NOT NULL, 
   created_by_id INT NOT NULL, 
   modified_by_id INT DEFAULT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (country_id)
+  last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY  (registration_id)
 );
-
-CREATE TABLE IF NOT EXISTS city (
-  city_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  city VARCHAR(50) NOT NULL,
-  country_id SMALLINT UNSIGNED NOT NULL,
-  created_timestamp DATETIME NOT NULL, 
-  created_by_id INT NOT NULL, 
-  modified_by_id INT DEFAULT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (city_id),
-  KEY idx_fk_country_id (country_id),
-  CONSTRAINT fk_city_country FOREIGN KEY (country_id) REFERENCES country (country_id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS address (
-  address_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  address VARCHAR(50) NOT NULL,
-  address2 VARCHAR(50) DEFAULT NULL,
-  district VARCHAR(20) NOT NULL,
-  city_id SMALLINT UNSIGNED NOT NULL,
-  postal_code VARCHAR(10) DEFAULT NULL,
-  phone VARCHAR(20) NOT NULL,
-  created_timestamp DATETIME NOT NULL, 
-  created_by_id INT NOT NULL, 
-  modified_by_id INT DEFAULT NULL,
-  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (address_id),
-  KEY idx_fk_city_id (city_id),
-  CONSTRAINT fk_address_city FOREIGN KEY (city_id) REFERENCES city (city_id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
 CREATE TABLE IF NOT EXISTS customer (
   customer_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(45) NOT NULL,
   last_name VARCHAR(45) NOT NULL,
   mobile_no VARCHAR(20) NOT NULL,
   email VARCHAR(50) DEFAULT NULL,
-  address_id SMALLINT UNSIGNED NOT NULL,
+  address VARCHAR(200)  NULL,
+  registration_id SMALLINT UNSIGNED NULL,
   created_timestamp DATETIME NOT NULL, 
   created_by_id INT NOT NULL, 
   modified_by_id INT DEFAULT NULL,
   last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY  (customer_id),
-  KEY idx_fk_address_id (address_id),
-  KEY idx_last_name (last_name),
-  CONSTRAINT fk_customer_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  KEY idx_fk_last_name (registration_id),
+  CONSTRAINT fk_customer_registration FOREIGN KEY (registration_id) REFERENCES loyal_customer (registration_id) ON DELETE RESTRICT ON UPDATE CASCADE
+
 );
 
 CREATE TABLE IF NOT EXISTS hotels(
@@ -70,54 +38,24 @@ CREATE TABLE IF NOT EXISTS hotels(
   modified_by_id INT DEFAULT NULL,
   last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (hotel_id),
-  KEY idx_fk_address_id (address_id) ,
-  CONSTRAINT fk_hotels_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-CREATE TABLE  IF NOT EXISTS rooms_type(
-  room_type VARCHAR(45) NOT NULL,
-  room_name VARCHAR(45) NOT NULL,
-  amount INT UNSIGNED NOT NULL ,
-  created_timestamp DATETIME NOT NULL, 
-  created_by_id INT NOT NULL, 
-  modified_by_id INT DEFAULT NULL,
-  last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (room_type)
-    
-);
-
-CREATE TABLE IF NOT EXISTS room(
-  room_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  room_type VARCHAR(45) NOT NULL,
-  room_description TEXT DEFAULT NULL,
-  no_accomodate SMALLINT UNSIGNED NOT NULL,
-  hotel_id SMALLINT UNSIGNED NOT NULL,
-  created_timestamp DATETIME NOT NULL, 
-  created_by_id INT NOT NULL, 
-  modified_by_id INT DEFAULT NULL,
-  last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (room_id,room_type),
-  KEY idx_fk_hotel_id (hotel_id),
-  KEY idx_fk_room_type(room_type),
-  CONSTRAINT fk_room_rooms_type FOREIGN KEY (room_type) REFERENCES rooms_type (room_type) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_room_hotels FOREIGN KEY (hotel_id) REFERENCES hotels (hotel_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  KEY idx_address_id (name)
 );
 
 
 CREATE TABLE IF NOT EXISTS booking(
   booking_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   customer_id SMALLINT UNSIGNED NOT NULL,
-  room_id SMALLINT UNSIGNED NOT NULL ,
+  hotel_id SMALLINT UNSIGNED NOT NULL,
   booking_date DATETIME NOT NULL ,
-  check_in_date DATETIME NOT NULL,
-  check_out_date DATETIME NOT NULL,
+  arrival_date DATETIME NOT NULL ,
+  departure_date DATETIME NOT NULL ,
   created_timestamp DATETIME NOT NULL, 
   created_by_id INT NOT NULL, 
   modified_by_id INT DEFAULT NULL,
   last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY  (booking_id),
-  KEY idx_fk_hotel_id (room_id),
-  KEY idx_fk_room_type (customer_id),
-  CONSTRAINT fk_booking_customer FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_booking_room FOREIGN KEY (room_id) REFERENCES room (room_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  KEY idx_fk_hotel_id (hotel_id),
+  CONSTRAINT UNIQUE uk_customer_id(customer_id),
+  CONSTRAINT fk_hotel_customer_customer FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_hotel_customer_hotels FOREIGN KEY (hotel_id) REFERENCES hotels (hotel_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
